08cbeff4654c1ea6ef50557790aa3dd88e41aae8
ABeldams <87949987+ABeldams@users.noreply.github.com>
2025-06-17 14:55:35 +0100

    Add files via upload

---
+++ src/assets/CardLogo.vue
@@ -0,0 +1,15 @@
+<template>
+  
+    <div class="h-100 cart-logo ">
+      <span class="cart-count">{{ cartCount }}</span>
+      🛒
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name:'CardLogo',
+    props: ['cartCount'],
+  };
+  </script>
+  
